ac67d87f33dcebd02cd2489743b1ce2da49f3729
hpipou <mr.aoudjit@gmail.com>
2023-05-18 11:06:30 +0200

    LOGUER TOUTES LES REQUETES DANS UN FICHIER LOGS logs.txt

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ const bodyParser    = require("body-parser")
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
+// enregistrer tous les logs dans un fichier text
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const logFilePath = path.join(__dirname, 'logs.txt');
+
+// Créer un flux de sortie vers le fichier logs.txt
+const accessLogStream = fs.createWriteStream(logFilePath, { flags: 'a' });
+
+// Définir le format de log souhaité
+const logFormat = ':remote-addr     :remote-user   [:date[clf]]   ":method   :url"';
+app.use(morgan(logFormat, { stream: accessLogStream }));
+
 // Limitation de requettes et protection contre les attaques DDOS
 const rateLimit = require('express-rate-limit')
 const limiter = rateLimit({
-	windowMs: 10 * 60 * 3000, // 15 minutes
-	max: 100, // Limiter chaque adresse ip à 100 requette pour 15min
+	windowMs: 10 * 60 * 3000, // 10 minutes
+	max: 100, // Limiter chaque adresse ip à 100 requette pour 10min
 	standardHeaders: true, 
 	legacyHeaders: false, 
 })
